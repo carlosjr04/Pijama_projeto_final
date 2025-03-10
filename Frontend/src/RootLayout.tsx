9c5f5c018ca68d194ab11e29c5edfa456200a2ef
Jade <jdmsa3@gmail.com>
2025-03-10 16:07:51 -0300

    fix: adição do componente Outlet no RootLayout + sem esse componente, os conteúdos que deveriam aparecer na tag <main> não aparecem

--- Frontend/src/RootLayout.tsx
+++ Frontend/src/RootLayout.tsx
@@ -1,12 +1,13 @@
 import Header from "./componentes/Header/Header";
 import Footer from "./componentes/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 export default function RootLayout(){
     return(
         <>
             <Header/>
             <main>
-                
+                <Outlet/>
             </main>
             <Footer/>
         </>
