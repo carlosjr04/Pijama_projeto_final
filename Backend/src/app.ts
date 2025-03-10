da150f05e64c12caf293ba8855084870c805364f
Arthur Pimenta <arthurstt22@gmail.com>
2025-03-10 16:19:51 -0300

    Feat: Adiciona rotas de feedbacks na aplicação.

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,38 +5,39 @@ import { env } from "./env";
 import fastifyCookie from "@fastify/cookie";
 import fastifyCors from "@fastify/cors";
 import { userRoutes } from "./http/controllers/user/routes";
-
+import { feedbacksRoutes } from "./http/controllers/feedback/routes";
 
 export const app = fastify();
 
 app.register(fastifyCors, {
-    origin: true,
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-})
+  origin: true,
+  methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+});
 
 app.register(fastifyJwt, {
-    secret: env.JWT_SECRET,
-    cookie: {
-        cookieName: 'refreshToken',
-        signed: false
-    },
-    sign: {
-        expiresIn: '10m'
-    }
-})
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
 
-app.register(fastifyCookie)
+app.register(fastifyCookie);
 
-app.register(userRoutes)
+app.register(userRoutes);
+app.register(feedbacksRoutes);
 
 app.setErrorHandler((error, request, reply) => {
-    if (error instanceof ZodError){
-        return reply.status(400).send({ message: 'validation error', issues: error.format()})
-    }
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "validation error", issues: error.format() });
+  }
 
-    return reply.status(500).send({ message: 'Internal server error'})
-})
-
-
+  return reply.status(500).send({ message: "Internal server error" });
+});
