68ec2d151019c773e2ea6110905dd224cf6e305a
Jade <jdmsa3@gmail.com>
2025-03-10 17:45:47 -0300

    feat(CartItem): adição de estilos

--- Frontend/src/componentes/CartItem/CartItem.tsx
+++ Frontend/src/componentes/CartItem/CartItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './styles.module.css'
 
 interface cartItemProps {
@@ -8,22 +9,45 @@ interface cartItemProps {
     price: number;
 }
 
-export default function CartItem({name, imgPath, code, size} : cartItemProps){
+export default function CartItem({name, imgPath, code, size, price} : cartItemProps){
+    const [quantity, setQuantity] = useState(1);
+    const inStock = 12;
+
+    function handleIncrement() {
+        if (quantity < inStock){
+            setQuantity(quantity + 1)
+        }
+    }
+
+    function handleDecrement() {
+        if (quantity > 1){
+            setQuantity(quantity - 1)
+        }
+    }
+    
     return(
         <div className={styles.cardContainer}>
             <img src={imgPath} alt=''></img>
-            <div className={styles.itemInfoGroup}>
-                <h4>{name}</h4>
-                <p>{code}</p>
-                <p>{size}</p>
 
+            <div className={styles.itemInfo}>
+                <div className={styles.nameAndCodeGroup}>
+                    <h4>{name}</h4>
+                    <h6>{code}</h6>
+                </div>
+                <p>{size}</p>
             </div>
-            <div className={styles.quantityAndTotalPriceGroup}>
+            
+            <div className={styles.quantityGroup}>
                 <p>Quantidade:</p>
-                {/* Botão de aumentar/diminuir quantidade */}
-                <p>Não perca sua oportunidade! Há apenas mais <span>X</span> peças disponíveis!</p>
-                <h2>R$ total</h2>
+                <div className={styles.inputGroup}>
+                    <button onClick={handleDecrement} className={styles.button}>-</button>
+                    <span className={styles.output}>{quantity}</span>
+                    <button onClick={handleIncrement} className={styles.button}>+</button>
+                </div>
+                <h6>Não perca sua oportunidade! Há apenas mais <span>{inStock}</span> peças disponíveis!</h6>
+                
             </div>
+            <h2>R$ {quantity * price}</h2>
             <div>
                 {/* Botão de excluir do carrinho */}
             </div>
