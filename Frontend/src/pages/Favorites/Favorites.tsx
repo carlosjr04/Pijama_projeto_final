c289338f2cdd22de895512b35e2a15c3a67406f7
Jade <jdmsa3@gmail.com>
2025-03-13 17:03:09 -0300

    feat: iniciando carrossel de favoritos

--- Frontend/src/pages/Favorites/Favorites.tsx
+++ Frontend/src/pages/Favorites/Favorites.tsx
@@ -1,25 +1,39 @@
 import FavItem from '../../componentes/FavItem/FavItem';
 import SectionHeader from '../../componentes/SectionHeader/SectionHeader'
 import useFavStore from '../../stores/FavStore'
+import styles from './styles.module.css'
+
+import leftArrow from '../../assets/anterior.png'
+import rightArrow from '../../assets/avancar.png'
 
 export default function Favorites() {
     const favorites = useFavStore((state) => state.favorites);
 
-    return(
+    return favorites.length > 0 ? (
         <>
             <SectionHeader currentPage="favorites"/>
-            <ul>
-                {favorites.map((item) => (
-                    <li key={item.id}>
-                        <FavItem
-                            id={item.id}
-                            name={item.name}
-                            imgPath={item.imgPath}
-                            price={item.price}
-                        />
-                    </li>
-                ))}
-            </ul>
+            <div className={styles.favContainer}>
+                <button>
+                        <img src={leftArrow} alt='seta para esquerda'/>
+                </button>
+                <ul>
+                    {favorites.map((item) => (
+                        <li key={item.id}>
+                            <FavItem
+                                id={item.id}
+                                name={item.name}
+                                imgPath={item.imgPath}
+                                price={item.price}
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <button>
+                    <img src={rightArrow} alt='seta para esquerda'/>
+                </button>
+            </div> 
         </>
-    )
+    ) : (
+        <h3>Você não tem pijamas favoritos</h3>
+    );
 }
