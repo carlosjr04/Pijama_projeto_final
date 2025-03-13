a8ee8bd5820c3cf3bfc6ca5405586334aab65a9b
Jade <jdmsa3@gmail.com>
2025-03-13 11:22:39 -0300

    fix: erros no CSS

--- Frontend/src/componentes/ModalButton/ModalButton.tsx
+++ Frontend/src/componentes/ModalButton/ModalButton.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 
 export default function ModalButton(props: {children: React.ReactNode, onClick: MouseEventHandler<HTMLButtonElement>}) {
     return(
-        <button onClick={props.onClick}className={styles.modalButton}>
+        <button onClick={props.onClick} className={styles.modalButton}>
             {props.children}
         </button>    
     )
